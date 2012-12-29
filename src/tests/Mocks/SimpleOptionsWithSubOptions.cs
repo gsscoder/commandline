d987b93a16bb5ecc9f7f3a1d82b28606ce8f0f56
Dan Nemec <djnemec@gmail.com>
2012-12-28 19:16:16 -0600

    Added support for SubOption parsers. Implements #17

---
+++ src/tests/Mocks/SimpleOptionsWithSubOptions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CommandLine.Tests.Mocks
+{
+    class SimpleOptionsWithSubOption : OptionsBase
+    {
+        [Option("s", "string")]
+        public string StringValue { get; set; }
+
+        [SubOption("suboption")]
+        public SimpleSubOptions SubOptions { get; set; }
+    }
+}
---
+++ src/tests/Mocks/SimpleSubOptions.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CommandLine.Tests.Mocks
+{
+    class SimpleSubOptions : OptionsBase
+    {
+        [Option("i", "int")]
+        public int IntegerValue { get; set; }
+    }
+}
